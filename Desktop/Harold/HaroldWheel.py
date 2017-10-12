7ea4f46379bf53cb2eb758833f1c78c5778ff8c2
Nicole Horward <nicole.horward@student.ehb.be>
2017-10-12 13:47:17 +0200

    Added calculations
    Calculate distance, speed and amount of rotations of the hamster wheel

--- Desktop/Harold/HaroldWheel.py
+++ Desktop/Harold/HaroldWheel.py
@@ -1,20 +1,43 @@
 import time
+import datetime
 import RPi.GPIO as io
 io.setmode(io.BCM)
 
+print("Hi, this is a tracker for Harold the Hamster")
+
+#Setup the pins of the Raspberry
 wheelpin = 17
-
-print('hi!')
-
 io.setup(wheelpin, io.IN, pull_up_down=io.PUD_UP)
 
-turns = 0
+# Circumferance of Harold's wheel
+# 18cm diameter * PI = 56.52 cm = 0.5652 m
+wheelsize = 0.5652
+
+# Number of wheel rotations
+rotations = 0
+
+# Distance covered in hamsterwheel
+distance = 0
+
+# Set the starttime to now
+starttime = datetime.datetime.now()
+
+
+# Function to calculate the current speed of the hamsterwheel
+def calculateSpeed(spintime):
+    global wheelsize
+    seconds = spintime.days * 24 * 60 * 60 + spintime.seconds + spintime.microseconds / 1000000.
+    return wheelsize / (seconds/60./60.)
 
 while True:
         if (io.input(wheelpin) == 0):
-            turns += 1
-            print(turns)
-        
-        time.sleep(0.1)
+            endtime = datetime.datetime.now()
+            spintime = endtime - starttime
+            starttime = endtime
+            speed = calculateSpeed(spintime)
+            distance = distance + wheelsize
+            rotations += 1
+            print(datetime.datetime.now, distance, speed, rotations)
+
 
         
